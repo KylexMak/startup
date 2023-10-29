d6341c88c4913b036b540a964e61f0a533d8d98e
KyleMak <khm45@byu.edu>
2023-10-28 19:42:07 -0600

    Fixed the form js

--- form.js
+++ form.js
@@ -1,5 +1,6 @@
-class Recipe{
-    constructor(dishName, dishIngredents, dishSteps){
-
-    }
-}
+const dishName = document.querySelector("#dishName");
+const ingredients = document.querySelector("#ingredients");
+const steps = document.querySelector(".steps");
+localStorage.setItem("DishName", dishName.value);
+localStorage.setItem("Ingredients", ingredients.value);
+localStorage.setItem("Steps", steps.value);
