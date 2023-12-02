f81b19826b94ad33d4725cccfe91bb4ba1bd4263
KyleMak <khm45@byu.edu>
2023-12-01 18:57:24 -0700

    Implemented websocket chat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const { WebSocketServer } = require('ws');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
@@ -8,6 +9,9 @@ const authCookieName = 'token';
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+server = app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
@@ -96,22 +100,69 @@ apiRouter.post('/recipe', (req, res) => {
 
 app.use(function (err, req, res, next) {
     res.status(500).send({ type: err.name, message: err.message });
-  });
-  
-  // Return the application's default page if the path is unknown
-  app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
-  
-  // setAuthCookie in the HTTP response
-  function setAuthCookie(res, authToken) {
-    res.cookie(authCookieName, authToken, {
-      secure: true,
-      httpOnly: true,
-      sameSite: 'strict',
-    });
-  }
-
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
 });
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+const wss = new WebSocketServer({ noServer: true });
+
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request);
+    });
+});
+
+let connections = [];
+
+wss.on('connection', (ws) => {
+    const connection = { id: connections.length + 1, alive: true, ws: ws };
+    connections.push(connection);
+
+    // Forward messages to everyone except the sender
+    ws.on('message', function message(data) {
+        connections.forEach((c) => {
+            if (c.id !== connection.id) {
+                c.ws.send(data);
+            }
+        });
+    });
+
+    // Remove the closed connection so we don't try to forward anymore
+    ws.on('close', () => {
+        connections.findIndex((o, i) => {
+            if (o.id === connection.id) {
+                connections.splice(i, 1);
+                return true;
+            }
+        });
+    });
+
+    // Respond to pong messages by marking the connection alive
+    ws.on('pong', () => {
+        connection.alive = true;
+    });
+});
+
+setInterval(() => {
+    connections.forEach((c) => {
+        // Kill any connection that didn't respond to the ping last time
+        if (!c.alive) {
+            c.ws.terminate();
+        } else {
+            c.alive = false;
+            c.ws.ping();
+        }
+    });
+}, 10000);
--- public/instructions.js
+++ public/instructions.js
@@ -90,3 +90,51 @@ function DisplaySteps(listOfSteps){
     });
     return ret_string;
 }
+
+const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+socket.onopen = (event) => {
+    appendMsg('system', 'websocket', 'connected');
+  };
+  
+  // Display messages we receive from our friends
+  socket.onmessage = async (event) => {
+    const text = await event.data.text();
+    const chat = JSON.parse(text);
+    appendMsg('friend', chat.name, chat.msg);
+  };
+  
+  // If the webSocket is closed then disable the interface
+  socket.onclose = (event) => {
+    appendMsg('system', 'websocket', 'disconnected');
+  };
+  
+  // Send a message over the webSocket
+  function sendMessage() {
+    const msgEl = document.querySelector('#comment');
+    const msg = msgEl.value;
+    if (!!msg) {
+      appendMsg('me', 'me', msg);
+      const name = Username.textContent;
+      socket.send(`{"name":"${name}", "msg":"${msg}"}`);
+      msgEl.value = '';
+    }
+  }
+  
+  // Create one long list of messages
+  function appendMsg(cls, from, msg) {
+    const chatText = document.querySelector('#chat-text');
+    chatText.innerHTML =
+      `<div><span class="${cls}">${from}</span>: ${msg}</div>` +
+      chatText.innerHTML;
+  }
+  
+  // Send message on enter keystroke
+  const input = document.querySelector('#comment');
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  });
+  
